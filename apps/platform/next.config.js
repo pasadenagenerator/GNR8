fc1ce5a7ae6633a440f8a7b34133d91af19e0f93
GregaZ <info@pasadenagenerator.com>
2026-02-21 22:50:12 +0100

    popravki

---
+++ apps/platform/next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@gnr8/core', '@gnr8/data'],
+}
+
+module.exports = nextConfig
--- packages/core/src/index.js
+++
@@ -1,6 +0,0 @@
-export * from './service-contract';
-export * from './modules/organization';
-export * from './modules/authorization';
-export * from './modules/project';
-export * from './modules/entitlement';
-export * from './modules/billing';
