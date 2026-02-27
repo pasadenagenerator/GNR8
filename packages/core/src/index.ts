2aff0e8626c1ae2233e25d3e68745dcd446824b3
GregaZ <info@pasadenagenerator.com>
2026-02-27 13:45:32 +0100

    superadmin audit enforcement 3

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,7 @@
+// ---- platform law / shared errors ----
 export * from './service-contract'
+
+// ---- core domain modules ----
 export * from './modules/organization'
 export * from './modules/authorization'
 export * from './modules/project'
@@ -6,6 +9,8 @@ export * from './modules/entitlement'
 export * from './modules/billing'
 export * from './modules/org-stats'
 export * from './modules/audit-log'
+
+// ---- superadmin modules (bypass layer) ----
 export * from './modules/superadmin-org'
 export * from './modules/superadmin-billing'
 export * from './modules/superadmin-trial'
